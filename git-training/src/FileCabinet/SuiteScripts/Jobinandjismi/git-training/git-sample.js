ed4306d780298bd58ffcc9fa238c4b5156335e6b
ginuselestin <112851348+ginuselestin@users.noreply.github.com>
2023-11-21 15:44:52 +0530

    OTP-5211-changed

--- git-training/src/FileCabinet/SuiteScripts/Jobinandjismi/git-training/git-sample.js
+++ git-training/src/FileCabinet/SuiteScripts/Jobinandjismi/git-training/git-sample.js
@@ -6,20 +6,8 @@ define(['N/record'],
     /**
  * @param{record} record
  */
-    (record) => {
-        /**
-         * Defines the function definition that is executed before record is loaded.
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.newRecord - New record
-         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-         * @param {Form} scriptContext.form - Current form
-         * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
-         * @since 2015.2
-         */
-        const beforeLoad = (scriptContext) => {
-
-        }
-
+  
+    
         /**
          * Defines the function definition that is executed before record is submitted.
          * @param {Object} scriptContext
